59477f432743320d89236b5ca9806155c25e57f2
Deshan <nawarathnadeshan@gmail.com>
2020-05-16 13:04:07 +0530

    After Adding TextField

--- lib/GPA1.dart
+++ lib/GPA1.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+
 
 import 'package:flutter/material.dart';
 
@@ -11,48 +11,136 @@ class _GPA1State extends State<GPA1> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-              body: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                   Column(children: <Widget>[
-            Flexible(
-                  child: Container(
-             width: 100,
-             child: TextFormField(
-  decoration: const InputDecoration(
-    
-  ))
-        ,
-           ),
-         )
-          ],),
-          Column(children: <Widget>[
-            Flexible(
-                  child: Container(
-             width: 100,
-             child: TextFormField(
-  decoration: const InputDecoration(
-    
-  ))
-        ,
-           ),
-         )
-          ],),
-           Column(children: <Widget>[
-            Flexible(
-                  child: Container(
-             width: 100,
-             child: TextFormField(
-  decoration: const InputDecoration(
-    
-  )),
-        
-           ),
-         )
-          ],)
-         ],),
-      )
-    );
+        child: Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+               Column(children: <Widget>[
+                 Text('Your Email : ',style: TextStyle(fontSize: 20.0),),
+               Text('Your GPA',style: TextStyle(fontSize: 20.0),),
+               Text('Number Of Credit',style: TextStyle(fontSize: 20.0),)
+               ],)
+                ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                  Text("Course Code",style: TextStyle(fontSize: 16),),
+                     Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                    
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Grade",style: TextStyle(fontSize: 16),),
+                   Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                    
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Credit",style: TextStyle(fontSize: 16),),
+                   Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                       Container(
+                        width: 100,
+                        child:
+                            TextFormField(decoration: const InputDecoration()),
+                      ),
+                    
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
